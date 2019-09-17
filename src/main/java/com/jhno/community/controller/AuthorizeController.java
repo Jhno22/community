49fc660f27a2071586436b1f842eccab1cdeb19e
tajkun <tajkun@163.com>
2019-09-17 19:05:48 +0800

    清除登录缓存

--- src/main/java/com/jhno/community/controller/AuthorizeController.java
+++ src/main/java/com/jhno/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -34,7 +36,8 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code")String code,
                            @RequestParam(name = "state")String state,
-                           HttpServletRequest request){
+                           HttpServletRequest request,
+                           HttpServletResponse response){
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
@@ -48,12 +51,15 @@ public class AuthorizeController {
         System.out.println(gitHubUser .getName()+gitHubUser.getId());
         if(gitHubUser  != null){
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(gitHubUser.getName());
             user.setAccountId(String.valueOf(gitHubUser.getId()));
             userMapper.insertSelective(user);
             //登录成功，写cookie和session
-            request.getSession().setAttribute("user",gitHubUser );
+            response.addCookie(new Cookie("token",token));
+
+//            request.getSession().setAttribute("user",gitHubUser );
             return "redirect:/";
         }else{
             //登录失败，重新登录
